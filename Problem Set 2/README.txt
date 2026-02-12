549037ef4d49d0d392241e9defe805ff1084bd40
Caio <caiogb85@gmail.com>
2026-02-12 19:38:26 -0300

    Problem Set 2
    Commit do problem set 2 do CS50.

---
+++ Problem Set 2/README.txt
@@ -0,0 +1,2 @@
+Esses são os exercícios do Problem Set 2 do CS50.
+"Preprocessing. Arrays. Strings. Command-Line Arguments. Cryptography."
---
+++ Problem Set 2/readability.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "cs50.h"
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+// Mostra o grade reading level da frase inserida
+int main(void)
+{
+    char *text = get_string("Text: ");
+    int textLen = strlen(text);
+
+    float words = 1;
+    float sentences;
+    float letters;
+
+    // Percorre pelo texto inserido
+    for (int i = 0; i < textLen; i++)
+    {   
+        // Se encontramos um espaço, incremente words
+        if (text[i] == ' ')
+        {
+            words++;
+        }
+        // Se encontrarmos algum terminador de frase, incremente sentences
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+        // Se encontrarmos algo alfabético, incremente letras 
+        else if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+
+    // Calcula o average letters per words
+    float avgL = (letters / words) * 100;
+    // Calcula o average setences per word
+    float avgS = (sentences / words) * 100;
+
+    // Calcula o index a partir o avgL e avgS
+    int index = round(0.0588 * avgL - 0.296 * avgS - 15.8);
+
+    // A partir do index, printa o grade reading level
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
